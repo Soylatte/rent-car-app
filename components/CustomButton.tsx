1068a41b09d12e9bcb27f192287466d0b091f2d4
Soyeon Kim <kksoyeon61@gmail.com>
2024-02-02 23:39:25 +0100

    add via upload

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,7 +2,7 @@
 import { CustomButtonProps } from "@/types"
 import Image from "next/image"
 
-const CustomButton = ({title, containerStyles, handleClick, btnType}:
+const CustomButton = ({title, containerStyles, handleClick, btnType, textStyles, rightIcon}:
     CustomButtonProps) => {
     return(
         <button className={`custom-btn ${containerStyles}`}
@@ -10,9 +10,16 @@ const CustomButton = ({title, containerStyles, handleClick, btnType}:
         disabled={false}
         onClick={handleClick}
         >
-            <span className={`flex-1`}>
+            <span className={`flex-1 ${textStyles}`}>
                 {title}
             </span>
+            {rightIcon && (
+                <div className="relative w-6 h-6">
+                    <Image src={rightIcon}
+                    alt="right icon"
+                    fill className="object-contain"/>
+                </div>
+            )}
         </button>
     )
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars(){
     const headers = {
             'X-RapidAPI-Key': '6d8bff3902msha11db9ec3ecb607p18f392jsnd8e858cc5546',
@@ -25,3 +27,17 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     return rentalRatePerDay.toFixed(0);
   };
   
+export const generateCarImageUrl = (car: CarProps, angle ? :string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(" ")[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+}
