48fae8b7b2af4781bbb9eb4d885b9fe8057ee448
Soyeon Kim <kksoyeon61@gmail.com>
2024-02-02 23:48:15 +0100

    add via upload

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,8 +1,10 @@
+"use client";
 import { Fragment } from 'react';
 import Image from 'next/image';
 
 import { Dialog, Transition } from '@headlessui/react';
 import { CarProps } from '@/types';
+import { generateCarImageUrl } from '@/utils';
 
 
 
@@ -50,26 +52,26 @@ const CarDetails = ({ isOpen, closeModal, car}: CarDetailsProps) => {
                     <div className='flex-1 flex flex-col gap-3'>
                         <div className='relative w-full h-40 bg-pattern bg-cover bg-center
                         rounded-lg'>
-                            <Image src='/hero.png'
+                            <Image src={generateCarImageUrl(car,'angle')}
             alt="car model" fill priority className="object-contain"/>
 
                         </div>
                         <div className='flex gap-3'>
                             <div className='flex-1 relative w-full h-24 bg-primary-blue-100
                             rounded-lg'>
-                            <Image src='/hero.png'
+                            <Image src={generateCarImageUrl(car,'angle')}
             alt="car model" fill priority className="object-contain"/>
 
                             </div>
                             <div className='flex-1 relative w-full h-24 bg-primary-blue-100
                             rounded-lg'>
-                            <Image src='/hero.png'
+                            <Image src={generateCarImageUrl(car,'angle')}
             alt="car model" fill priority className="object-contain"/>
 
                             </div>
                             <div className='flex-1 relative w-full h-24 bg-primary-blue-100
                             rounded-lg'>
-                            <Image src='/hero.png'
+                            <Image src={generateCarImageUrl(car,'angle')}
             alt="car model" fill priority className="object-contain"/>
                             </div>
                         </div>
